846329b863ffe7dcb8b483c0bde2b8ee6e23507f
Michael Soffer <77414836+michaelsoffer@users.noreply.github.com>
2022-01-24 00:54:35 -0800

    hamburger menu function for mobile

---
+++ app.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var x = document.getElementById("top-nav-rightside");
+    if (x.className === "top-nav-right") {
+      x.className += " responsive";
+    } else {
+      x.className = "top-nav-right";
+    }
+  }
